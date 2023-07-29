108c0ff9e36f6bb86a3d431aa5e8556134989b66
Polly9 <polly9099@gmail.com>
2023-07-29 20:20:26 +0000

    test

--- gcd.sh
+++ gcd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ $1 -lt 1 ];then
-    echo err
+    echo error
     exit 1
 elif [ $2 -lt 1 ];then
-    echo err
+    echo error
     exit 1
 else
     comm -1 -2 <(factor $1|sed  's/ /\n/g'|sort) <(factor $2|sed 's/ /\n/g'|sort) | awk 'BEGIN{a=1}{a*=$1}END{print a}'
